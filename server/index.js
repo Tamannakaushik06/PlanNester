e2cd040a2667176221c1cdb701931dd1d9188f6e
Tamanna <112933240+Tamannakaushik06@users.noreply.github.com>
2024-12-01 21:17:39 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,37 +1,10 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
 import express from "express";
-import morgan from "morgan";
-import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewaves.js";
-import routes from "./routes/index.js";
-import { dbConnection } from "./utils/index.js";
+import userRoutes from "./userRoutes.js";
+import taskRoutes from "./taskRoutes.js";
 
-dotenv.config();
+const router = express.Router();
 
-dbConnection();
+router.use("/user", userRoutes); //api/user/login
+router.use("/task", taskRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-const app = express();
-
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-
-app.use(morgan("dev"));
-app.use("/api", routes);
-
-app.use(routeNotFound);
-app.use(errorHandler);
-
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+export default router;
